81a98b4034afee71ff87401d6886ad12208c00e4
inesmgraca <64962579+inesmgraca@users.noreply.github.com>
2021-03-11 13:31:46 +0000

    Jogo do Galo

---
+++ JogoGalo/JogoGalo/JogoGalo.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace JogoGalo
+{
+    partial class JogoGalo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JogoGalo));
+            this.btnIniciarJogo = new System.Windows.Forms.Button();
+            this.btnPosicao1 = new System.Windows.Forms.Button();
+            this.btnPosicao2 = new System.Windows.Forms.Button();
+            this.btnPosicao3 = new System.Windows.Forms.Button();
+            this.btnPosicao4 = new System.Windows.Forms.Button();
+            this.btnPosicao5 = new System.Windows.Forms.Button();
+            this.btnPosicao6 = new System.Windows.Forms.Button();
+            this.btnPosicao7 = new System.Windows.Forms.Button();
+            this.btnPosicao8 = new System.Windows.Forms.Button();
+            this.btnPosicao9 = new System.Windows.Forms.Button();
+            this.txtEndGame = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnIniciarJogo
+            // 
+            this.btnIniciarJogo.Font = new System.Drawing.Font("Arial Nova Light", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciarJogo.Location = new System.Drawing.Point(481, 70);
+            this.btnIniciarJogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnIniciarJogo.Name = "btnIniciarJogo";
+            this.btnIniciarJogo.Size = new System.Drawing.Size(219, 47);
+            this.btnIniciarJogo.TabIndex = 0;
+            this.btnIniciarJogo.Text = "Iniciar Jogo";
+            this.btnIniciarJogo.UseVisualStyleBackColor = true;
+            this.btnIniciarJogo.Click += new System.EventHandler(this.btnIniciarJogo_Click);
+            // 
+            // btnPosicao1
+            // 
+            this.btnPosicao1.Enabled = false;
+            this.btnPosicao1.Font = new System.Drawing.Font("Arial Nova", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPosicao1.Location = new System.Drawing.Point(44, 47);
+            this.btnPosicao1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPosicao1.Name = "btnPosicao1";
+            this.btnPosicao1.Size = new System.Drawing.Size(88, 94);
+            this.btnPosicao1.TabIndex = 1;
+            this.btnPosicao1.UseVisualStyleBackColor = true;
+            this.btnPosicao1.Click += new System.EventHandler(this.btnPosicao1_Click);
+            // 
+            // btnPosicao2
+            // 
+            this.btnPosicao2.Enabled = false;
+            this.btnPosicao2.Font = new System.Drawing.Font("Arial Nova", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPosicao2.Location = new System.Drawing.Point(175, 47);
+            this.btnPosicao2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPosicao2.Name = "btnPosicao2";
+            this.btnPosicao2.Size = new System.Drawing.Size(88, 94);
+            this.btnPosicao2.TabIndex = 2;
+            this.btnPosicao2.UseVisualStyleBackColor = true;
+            this.btnPosicao2.Click += new System.EventHandler(this.btnPosicao2_Click);
+            // 
+            // btnPosicao3
+            // 
+            this.btnPosicao3.Enabled = false;
+            this.btnPosicao3.Font = new System.Drawing.Font("Arial Nova", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPosicao3.Location = new System.Drawing.Point(306, 47);
+            this.btnPosicao3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPosicao3.Name = "btnPosicao3";
+            this.btnPosicao3.Size = new System.Drawing.Size(88, 94);
+            this.btnPosicao3.TabIndex = 3;
+            this.btnPosicao3.UseVisualStyleBackColor = true;
+            this.btnPosicao3.Click += new System.EventHandler(this.btnPosicao3_Click);
+            // 
+            // btnPosicao4
+            // 
+            this.btnPosicao4.Enabled = false;
+            this.btnPosicao4.Font = new System.Drawing.Font("Arial Nova", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPosicao4.Location = new System.Drawing.Point(44, 164);
+            this.btnPosicao4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPosicao4.Name = "btnPosicao4";
+            this.btnPosicao4.Size = new System.Drawing.Size(88, 94);
+            this.btnPosicao4.TabIndex = 4;
+            this.btnPosicao4.UseVisualStyleBackColor = true;
+            this.btnPosicao4.Click += new System.EventHandler(this.btnPosicao4_Click);
+            // 
+            // btnPosicao5
+            // 
+            this.btnPosicao5.Enabled = false;
+            this.btnPosicao5.Font = new System.Drawing.Font("Arial Nova", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPosicao5.Location = new System.Drawing.Point(175, 164);
+            this.btnPosicao5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPosicao5.Name = "btnPosicao5";
+            this.btnPosicao5.Size = new System.Drawing.Size(88, 94);
+            this.btnPosicao5.TabIndex = 5;
+            this.btnPosicao5.UseVisualStyleBackColor = true;
+            this.btnPosicao5.Click += new System.EventHandler(this.btnPosicao5_Click);
+            // 
+            // btnPosicao6
+            // 
+            this.btnPosicao6.Enabled = false;
+            this.btnPosicao6.Font = new System.Drawing.Font("Arial Nova", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPosicao6.Location = new System.Drawing.Point(306, 164);
+            this.btnPosicao6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPosicao6.Name = "btnPosicao6";
+            this.btnPosicao6.Size = new System.Drawing.Size(88, 94);
+            this.btnPosicao6.TabIndex = 6;
+            this.btnPosicao6.UseVisualStyleBackColor = true;
+            this.btnPosicao6.Click += new System.EventHandler(this.btnPosicao6_Click);
+            // 
+            // btnPosicao7
+            // 
+            this.btnPosicao7.Enabled = false;
+            this.btnPosicao7.Font = new System.Drawing.Font("Arial Nova", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPosicao7.Location = new System.Drawing.Point(44, 281);
+            this.btnPosicao7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPosicao7.Name = "btnPosicao7";
+            this.btnPosicao7.Size = new System.Drawing.Size(88, 94);
+            this.btnPosicao7.TabIndex = 7;
+            this.btnPosicao7.UseVisualStyleBackColor = true;
+            this.btnPosicao7.Click += new System.EventHandler(this.btnPosicao7_Click);
+            // 
+            // btnPosicao8
+            // 
+            this.btnPosicao8.Enabled = false;
+            this.btnPosicao8.Font = new System.Drawing.Font("Arial Nova", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPosicao8.Location = new System.Drawing.Point(175, 281);
+            this.btnPosicao8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPosicao8.Name = "btnPosicao8";
+            this.btnPosicao8.Size = new System.Drawing.Size(88, 94);
+            this.btnPosicao8.TabIndex = 8;
+            this.btnPosicao8.UseVisualStyleBackColor = true;
+            this.btnPosicao8.Click += new System.EventHandler(this.btnPosicao8_Click);
+            // 
+            // btnPosicao9
+            // 
+            this.btnPosicao9.Enabled = false;
+            this.btnPosicao9.Font = new System.Drawing.Font("Arial Nova", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPosicao9.Location = new System.Drawing.Point(306, 281);
+            this.btnPosicao9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPosicao9.Name = "btnPosicao9";
+            this.btnPosicao9.Size = new System.Drawing.Size(88, 94);
+            this.btnPosicao9.TabIndex = 9;
+            this.btnPosicao9.UseVisualStyleBackColor = true;
+            this.btnPosicao9.Click += new System.EventHandler(this.btnPosicao9_Click);
+            // 
+            // txtEndGame
+            // 
+            this.txtEndGame.Enabled = false;
+            this.txtEndGame.Font = new System.Drawing.Font("Arial Nova Light", 14F);
+            this.txtEndGame.Location = new System.Drawing.Point(481, 164);
+            this.txtEndGame.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtEndGame.Multiline = true;
+            this.txtEndGame.Name = "txtEndGame";
+            this.txtEndGame.Size = new System.Drawing.Size(219, 211);
+            this.txtEndGame.TabIndex = 10;
+            // 
+            // JogoGalo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 432);
+            this.Controls.Add(this.txtEndGame);
+            this.Controls.Add(this.btnPosicao9);
+            this.Controls.Add(this.btnPosicao8);
+            this.Controls.Add(this.btnPosicao7);
+            this.Controls.Add(this.btnPosicao6);
+            this.Controls.Add(this.btnPosicao5);
+            this.Controls.Add(this.btnPosicao4);
+            this.Controls.Add(this.btnPosicao3);
+            this.Controls.Add(this.btnPosicao2);
+            this.Controls.Add(this.btnPosicao1);
+            this.Controls.Add(this.btnIniciarJogo);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "JogoGalo";
+            this.Text = "Jogo do Galo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciarJogo;
+        private System.Windows.Forms.Button btnPosicao1;
+        private System.Windows.Forms.Button btnPosicao2;
+        private System.Windows.Forms.Button btnPosicao3;
+        private System.Windows.Forms.Button btnPosicao4;
+        private System.Windows.Forms.Button btnPosicao5;
+        private System.Windows.Forms.Button btnPosicao6;
+        private System.Windows.Forms.Button btnPosicao7;
+        private System.Windows.Forms.Button btnPosicao8;
+        private System.Windows.Forms.Button btnPosicao9;
+        private System.Windows.Forms.TextBox txtEndGame;
+    }
+}
+
---
+++ JogoGalo/JogoGalo/JogoGalo.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static JogoGalo.Regras;
+
+namespace JogoGalo
+{
+    public partial class JogoGalo : Form
+    {
+        JogoDePollo jogo;
+
+        public JogoGalo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciarJogo_Click(object sender, EventArgs e)
+        {
+            jogo = new JogoDePollo();
+            btnPosicao1.Enabled = true;
+            btnPosicao1.Text = "";
+            btnPosicao2.Enabled = true;
+            btnPosicao2.Text = "";
+            btnPosicao3.Enabled = true;
+            btnPosicao3.Text = "";
+            btnPosicao4.Enabled = true;
+            btnPosicao4.Text = "";
+            btnPosicao5.Enabled = true;
+            btnPosicao5.Text = "";
+            btnPosicao6.Enabled = true;
+            btnPosicao6.Text = "";
+            btnPosicao7.Enabled = true;
+            btnPosicao7.Text = "";
+            btnPosicao8.Enabled = true;
+            btnPosicao8.Text = "";
+            btnPosicao9.Enabled = true;
+            btnPosicao9.Text = "";
+            btnIniciarJogo.Text = "Reiniciar Jogo";
+        }
+
+        private void btnPosicao1_Click(object sender, EventArgs e)
+        {
+            posicaoJogar(1, btnPosicao1);
+        }
+
+        private void btnPosicao2_Click(object sender, EventArgs e)
+        {
+            posicaoJogar(2, btnPosicao2);
+        }
+
+        private void btnPosicao3_Click(object sender, EventArgs e)
+        {
+            posicaoJogar(3, btnPosicao3);
+        }
+
+        private void btnPosicao4_Click(object sender, EventArgs e)
+        {
+            posicaoJogar(4, btnPosicao4);
+        }
+
+        private void btnPosicao5_Click(object sender, EventArgs e)
+        {
+            posicaoJogar(5, btnPosicao5);
+        }
+
+        private void btnPosicao6_Click(object sender, EventArgs e)
+        {
+            posicaoJogar(6, btnPosicao6);
+        }
+
+        private void btnPosicao7_Click(object sender, EventArgs e)
+        {
+            posicaoJogar(7, btnPosicao7);
+        }
+
+        private void btnPosicao8_Click(object sender, EventArgs e)
+        {
+            posicaoJogar(8, btnPosicao8);
+        }
+
+        private void btnPosicao9_Click(object sender, EventArgs e)
+        {
+            posicaoJogar(9, btnPosicao9);
+        }
+
+        private void posicaoJogar(int posicaoJogada, Button btnJogada)
+        {
+            if (jogo != null)
+            {
+                var jogada = jogo.Jogar(posicaoJogada);
+
+                if (jogo.EndGame)
+                {
+                    if (jogada == EstadoJogo.Empate)
+                    {
+                        btnJogada.Text = jogo.Peca == "X" ? "O" : "X";
+                        txtEndGame.Text += "\nEmpate!";
+                    }
+                    if (jogada == EstadoJogo.Vitoria1)
+                    {
+                        btnJogada.Text = jogo.Peca;
+                        txtEndGame.Text += "\nVitória do Jogador X!";
+                    }
+                    if (jogada == EstadoJogo.Vitoria2)
+                    {
+                        btnJogada.Text = jogo.Peca;
+                        txtEndGame.Text += "\nVitória do Jogador O!";
+                    }
+
+                    jogo = null;
+                    btnPosicao1.Enabled = false;
+                    btnPosicao2.Enabled = false;
+                    btnPosicao3.Enabled = false;
+                    btnPosicao4.Enabled = false;
+                    btnPosicao5.Enabled = false;
+                    btnPosicao6.Enabled = false;
+                    btnPosicao7.Enabled = false;
+                    btnPosicao8.Enabled = false;
+                    btnPosicao9.Enabled = false;
+                }
+                else if (jogada == EstadoJogo.Valido)
+                {
+                    btnJogada.Text = jogo.Peca == "X" ? "O" : "X";
+                }
+            }
+        }
+    }
+}
